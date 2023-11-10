e1ed8ba549c45b731fcc3eeae864ec897d2e9d31
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-10 09:52:28 -0800

    03 setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import Home from "./pages/home";
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// export default class Home extends React.Component {
+//   render(){
+//     return(
+//       <button>click me! </button>
+//     )}}
+
+function Home(){
+  return(
+    <button>click me! </button>
+  )
+}
+export default Home;
